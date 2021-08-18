f7a517171a424051f85eadfd45b1b25778fa8638
Madhumitha <madhusedulous@gmail.com>
2021-08-18 07:17:49 -0700

    Requirement 1 : Calculation of area of Rectangle

--- src/main/java/Shapes.java
+++ src/main/java/Shapes.java
@@ -1,2 +1,19 @@
 public class Shapes {
+    int length;
+    int breadth;
+    int area()
+    {
+        return length*breadth;
+    }
+
+}
+class Rectangle extends Shapes
+{
+
+    Rectangle(int length,int breadth)
+    {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
 }
--- src/test/java/ShapesTest.java
+++ src/test/java/ShapesTest.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ShapesTest {
+
+    @Test
+    void toCalculateAreaOfaRectangleWithPositiveDimensions() {
+        Shapes rectangleObj = new Rectangle(2, 4);
+        int expectedarea = 8;
+        int calculatedarea = rectangleObj.area();
+        assertEquals(expectedarea, calculatedarea);
+    }
+
+    @Test
+    void toCalculateAreaOfaRectangleWithNegativeDimensions() {
+        Rectangle rectangleObj = new Rectangle(-2, -4);
+        int expectedarea = 8;
+        int calculatedarea = rectangleObj.area();
+        assertEquals(expectedarea, calculatedarea);
+    }
+
 }
